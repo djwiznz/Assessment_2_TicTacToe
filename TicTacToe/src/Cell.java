c2195f972255f9aee0e026f7a91d03d0494896ca
djwiznz <djwiznz@gmail.com>
2024-12-23 17:48:29 +1300

    Added Author Comments: Included author details and descriptions at the top of each file. Final Code Review and Cleanup: Ensured best practices, clean formatting, and robust validation.

--- TicTacToe/src/Cell.java
+++ TicTacToe/src/Cell.java
@@ -1,3 +1,9 @@
+/**
+ * Name: Wiremu Te Moni 
+ * Student ID: 5115927
+ * Course Code: BIT504
+ * Assessment 2
+ */
 public class Cell {
     Player content; // Content of the cell
     int row, col; // Row and column
